77c8e3e75908cafd480b300185809934e3eb408c
CAVEDU Education <nissin@cavedu.com>
2020-09-21 12:09:33 +0800

    update example
    with /arduino and /hangeekduino

---
+++ example/hangeekduino/Lesson_1_LED/Lesson_1_LED.ino
@@ -0,0 +1,24 @@
+/*
+ * Generated using BlocklyDuino:
+ *
+ * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
+ *
+ * Date: Mon, 21 Sep 2020 03:54:19 GMT
+ */
+
+
+
+void setup()
+{
+  pinMode(4, OUTPUT);
+
+}
+
+
+void loop()
+{
+  digitalWrite(4, 1);
+  delay(1000);
+  digitalWrite(4, 0);
+  delay(1000);
+}
